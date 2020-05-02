8c548c09b1df97ecf89d54dae88f73c982412bfa
Alex <code@alexheggie.space>
2020-05-02 20:31:36 +0000

    greyscale working, reflect partically working

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -1,8 +1,23 @@
+#include <stdio.h>
 #include "helpers.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int h = 0; h < height; h++) {
+        printf("line %i", h+1);
+        for (int w = 0; w < width; w++) {
+            RGBTRIPLE pixel = image[h][w];
+            BYTE greyscale = (pixel.rgbtGreen + pixel.rgbtRed + pixel.rgbtBlue) / 3;
+            // printf("%x%x%x ", pixel.rgbtRed, pixel.rgbtGreen, pixel.rgbtBlue);
+            image[h][w].rgbtRed = greyscale;
+            image[h][w].rgbtGreen = greyscale;
+            image[h][w].rgbtBlue = greyscale;
+
+        }
+        printf("\n");
+    }
+    // printf("h: %i w: %i", height, width);
     return;
 }
 
@@ -15,6 +30,24 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int h = 0; h < height; h++) {
+        printf("line %i", h+1);
+        for (int w = 0; w < width/2; w++) {
+            int opposite_w = width-w;
+
+            RGBTRIPLE pixel = image[h][w];
+            RGBTRIPLE opposite_pixel = image[h][opposite_w];
+
+            image[h][w].rgbtRed = opposite_pixel.rgbtRed;
+            image[h][w].rgbtGreen = opposite_pixel.rgbtGreen;
+            image[h][w].rgbtBlue = opposite_pixel.rgbtBlue;
+            
+            image[h][opposite_w].rgbtRed = pixel.rgbtRed;
+
+        }
+        printf("\n");
+    }
+    // printf("h: %i w: %i", height, width);
     return;
 }
 
