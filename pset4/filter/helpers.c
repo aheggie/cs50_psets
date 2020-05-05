797289492815b027e670699ddd48cca0df2ea81f
Alex <26582461+aheggie@users.noreply.github.com>
2020-05-05 20:42:04 +0000

    greyscale copied from non-compiling to clean file - compiling still works

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -3,8 +3,23 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int h = 0; h < height; h++) {
+        // printf("line %i", h+1);
+        for (int w = 0; w < width; w++) {
+            RGBTRIPLE pixel = image[h][w];
+            BYTE greyscale = round((pixel.rgbtGreen + pixel.rgbtRed + pixel.rgbtBlue) / 3.0);
+            // printf("%x%x%x ", pixel.rgbtRed, pixel.rgbtGreen, pixel.rgbtBlue);
+            image[h][w].rgbtRed = greyscale;
+            image[h][w].rgbtGreen = greyscale;
+            image[h][w].rgbtBlue = greyscale;
+
+        }
+        // printf("\n");
+    }
+    // printf("h: %i w: %i", height, width);
     return;
 }
+
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
