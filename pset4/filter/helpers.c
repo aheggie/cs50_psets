b9ffd0392823cb4485740ec580ed8cda53e8b414
Alex <26582461+aheggie@users.noreply.github.com>
2020-05-05 20:45:57 +0000

    reflect copied over - compiles in check and works offline but doesnt clear reflect checks?

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -30,6 +30,26 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int h = 0; h < height; h++) {
+        // printf("line %i", h+1);
+        for (int w = 0; w < width/2; w++) {
+            int opposite_w = width-w;
+
+            RGBTRIPLE pixel = image[h][w];
+            RGBTRIPLE opposite_pixel = image[h][opposite_w];
+
+            image[h][w].rgbtRed = opposite_pixel.rgbtRed;
+            image[h][w].rgbtGreen = opposite_pixel.rgbtGreen;
+            image[h][w].rgbtBlue = opposite_pixel.rgbtBlue;
+
+            image[h][opposite_w].rgbtRed = pixel.rgbtRed;
+            image[h][opposite_w].rgbtGreen = pixel.rgbtGreen;
+            image[h][opposite_w].rgbtBlue = pixel.rgbtBlue;
+
+        }
+        // printf("\n");
+    }
+    // printf("h: %i w: %i", height, width);
     return;
 }
 
