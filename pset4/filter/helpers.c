e98af89c0e324d4e8f15fab18229a8cee4465b8b
Alex <26582461+aheggie@users.noreply.github.com>
2020-05-05 20:52:46 +0000

    sepia copied over and clears tests on check50 - what was breaking compile?

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -22,8 +22,36 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 
 
 // Convert image to sepia
+BYTE max_255(int integer) {
+    return integer > 255 ? 255 : integer;
+}
+
+RGBTRIPLE sepia_pixel(RGBTRIPLE input_pixel) {
+    RGBTRIPLE output_pixel;
+    float originalRed = input_pixel.rgbtRed;
+    float originalGreen = input_pixel.rgbtGreen;
+    float originalBlue = input_pixel.rgbtBlue;
+    output_pixel.rgbtRed = max_255(round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue));
+    output_pixel.rgbtGreen = max_255(round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue));
+    output_pixel.rgbtBlue = max_255(round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue));
+
+    return output_pixel;
+}
+
+// Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int h = 0; h < height; h++) {
+        for (int w = 0; w < width; w++) {
+
+            RGBTRIPLE sepia = sepia_pixel(image[h][w]);
+
+            image[h][w].rgbtRed = sepia.rgbtRed;
+            image[h][w].rgbtGreen = sepia.rgbtGreen;
+            image[h][w].rgbtBlue = sepia.rgbtBlue;
+
+        }
+    }
     return;
 }
 
