f86b2f58a6f54c6fe42941e1cad043e442df62a7
Alex <26582461+aheggie@users.noreply.github.com>
2020-05-15 22:28:39 +0000

    error implemented for recover

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -6,6 +6,15 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        error()
+        return error("usage: ./recover filename\n");
     }
+    char *filepath = argv[1];
+    printf("%s\n", filepath);
 }
+
+int error(char *message)
+{
+    printf("%s", message);
+    return 1;
+}
+
